a8917976c1bb11969a92cc42c1f8a5db9151b133
mohammadreza <mmdrza77@gmail.com>
2024-01-07 21:01:29 +0330

    something changes

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import BasePage from "../src/components/base-page/base-page.jsx";
 import Image from "next/image";
 import { LandingLayout } from "@/layout/landing-layout/index.js";
-import Landing from "@/components/landing/landing.jsx";
+import Landing from "../src/components/landing/landing.jsx";
 export default function Home() {
   const [color, setColor] = useState("bg-teal-300");
   return (
