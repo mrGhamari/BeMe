a9a10a8c3518bccd1a60e5bdb7747d692d002720
mrGhamari <mmdrza77@gmail.com>
2024-01-08 23:45:25 +0330

    develop

--- src/components/landing/hostImage.jsx
+++ src/components/landing/hostImage.jsx
@@ -1,24 +1,19 @@
 import React from "react";
 import Image from "next/image";
-const imageStyle = {
-  width: "fit-content",
-  height: "fit-content",
-};
 const hostImage = ({ hostinformation }) => {
   return (
     <div
       className={`relative  ${hostinformation.fullWidth ? "col-span-4" : ""}`}
     >
-      <div className="absolute size-full opacity-0 cursor-pointer hover:opacity-100 hover:bg-rgba flex flex-col justify-center items-center text-white text-3xl">
-        <h3>{hostinformation.name}</h3>
-        <p>{hostinformation.field}</p>
+      <div className="absolute size-full opacity-0 cursor-pointer hover:opacity-100 hover:bg-rgba flex flex-col justify-center items-center text-white">
+        <div className="text-3xl sm:text-sm">{hostinformation.name}</div>
+        <div className="text-xl text-teal-200">{hostinformation.field}</div>
       </div>
       <Image
         src={hostinformation.photo}
         alt="photo"
         className="rounded-3xl size-full"
-        quality={80}
-        style={imageStyle}
+        quality={70}
       />
     </div>
   );
--- src/components/landing/images.jsx
+++ src/components/landing/images.jsx
@@ -9,8 +9,6 @@ const imageStyle = {
   width: "fit-content",
   height: "fit-content",
 };
-
-const Images = () => {
   const hostList = [
     {
       name: "ایمان حاجی‌علی",
@@ -43,67 +41,13 @@ const Images = () => {
       fullWidth: true,
     },
   ];
+const Images = () => {
+
   return (
-    <div className="grid grid-cols-4 gap-4">
+    <div className="grid sm:grid-cols-6 gap-4">
       {hostList.map((item, index) => (
         <Host key={index} hostinformation={item} />
       ))}
-
-      {/* <div className="relative">
-        <div className="absolute size-full opacity-0 cursor-pointer hover:opacity-100 hover:bg-rgba flex flex-col justify-center items-center text-white text-3xl">
-          <h3>محمدرضا قمری</h3>
-          <p>برنامه‌نویس</p>
-        </div>
-        <Image
-          src={mmdrza}
-          alt="photo"
-          className="rounded-3xl size-full"
-          quality={80}
-          style={imageStyle}
-        />
-      </div>
-
-      <div className="relative">
-        <div className="absolute size-full opacity-0 cursor-pointer hover:opacity-100 hover:bg-rgba flex flex-col justify-center items-center text-white text-3xl">
-          <h3>دانیال محمدی</h3>
-          <p>معلم</p>
-        </div>
-        <Image
-          src={daniel}
-          alt="photo"
-          className="rounded-3xl size-full"
-          quality={80}
-          style={imageStyle}
-        />
-      </div>
-
-      <div className="relative col-span-3">
-        <div className="absolute size-full opacity-0 cursor-pointer hover:opacity-100 hover:bg-rgba flex flex-col justify-center items-center text-white text-3xl">
-          <h3>ایمان حاجی‌علی</h3>
-          <p>عکاس</p>
-        </div>
-        <Image
-          src={iman1}
-          alt="photo"
-          className="rounded-3xl size-full"
-          quality={100}
-          style={imageStyle}
-        />
-      </div>
-
-      <div className="relative">
-        <div className="absolute size-full opacity-0 cursor-pointer hover:opacity-100 hover:bg-rgba flex flex-col justify-center items-center text-white text-3xl">
-          <h3>دکتر صدرا صفوی</h3>
-          <p>دام‌پزشک</p>
-        </div>
-        <Image
-          src={sadra}
-          alt="photo"
-          className="rounded-3xl size-full"
-          quality={70}
-          style={imageStyle}
-        />
-      </div> */}
     </div>
   );
 };
