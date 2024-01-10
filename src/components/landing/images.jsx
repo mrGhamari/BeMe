7cccfd11ea344c664b74e95123d1f4980ad32dbc
dannymhmdi <dannymohammadi1377@gmail.com>
2024-01-10 10:12:33 +0330

    image size problem fixed

--- src/components/landing/images.jsx
+++ src/components/landing/images.jsx
@@ -9,46 +9,53 @@ const imageStyle = {
   width: "fit-content",
   height: "fit-content",
 };
-  const hostList = [
-    {
-      name: "ایمان حاجی‌علی",
-      field: "مکانیک",
-      photo: iman,
-      fullWidth: false,
-    },
-    {
-      name: "دانیال محمدی",
-      field: "معلم",
-      photo: daniel,
-      fullWidth: false,
-    },
-    {
-      name: "محمدرضا",
-      field: "برنامه‌نویس",
-      photo: mmdrza,
-      fullWidth: false,
-    },
-    {
-      name: "صدرا صفوی",
-      field: "دام‌پزشک",
-      photo: sadra,
-      fullWidth: false,
-    },
-    {
-      name: "پارسا بیکی",
-      field: "عکاس",
-      photo: iman1,
-      fullWidth: true,
-    },
-  ];
+const hostList = [
+  {
+    name: "ایمان حاجی‌علی",
+    field: "مکانیک",
+    photo: iman,
+    fullWidth: false,
+  },
+  {
+    name: "دانیال محمدی",
+    field: "معلم",
+    photo: daniel,
+    fullWidth: false,
+  },
+  {
+    name: "محمدرضا",
+    field: "برنامه‌نویس",
+    photo: mmdrza,
+    fullWidth: false,
+  },
+  {
+    name: "صدرا صفوی",
+    field: "دام‌پزشک",
+    photo: sadra,
+    fullWidth: false,
+  },
+  {
+    name: "پارسا بیکی",
+    field: "عکاس",
+    photo: iman1,
+    fullWidth: true,
+  },
+];
 const Images = () => {
-
   return (
-    <div className="grid sm:grid-cols-6 gap-4">
-      {hostList.map((item, index) => (
-        <Host key={index} hostinformation={item} />
-      ))}
-    </div>
+    <>
+      <div className="grid grid-cols-12 gap-4 border-4">
+        {hostList.map((item, index) => (
+          <Host key={index} hostinformation={item} />
+        ))}
+      </div>
+      {/* <div className="border-4 grid grid-cols-4">
+        <div className="border">01</div>
+        <div className="border">02</div>
+        <div className="border">03</div>
+        <div className="border">04</div>
+      </div> */}
+    </>
   );
 };
 export default Images;
