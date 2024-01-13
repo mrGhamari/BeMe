ecf0bf911358553f2ad9b4dbd6404b2aaac24d4d
miveh <miveh@imivechi@gmail.com>
2024-01-13 19:55:11 +0330

    create about's first component(FirstTextComponent)

--- src/components/about-us/PageContainer.jsx
+++ src/components/about-us/PageContainer.jsx
@@ -2,7 +2,7 @@ import { Container } from "@mui/material";
 import React from "react";
 
 const PageContainer = ({ children, fluid }) => {
-  return <Container maxWidth={fluid}>{children}</Container>;
+  return <Container maxWidth={"lg"}>{children}</Container>;
 };
 
 export default PageContainer;
