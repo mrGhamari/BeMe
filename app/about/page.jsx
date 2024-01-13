78258f89e283914fbfb2c891e792444c091242b0
miveh <miveh@imivechi@gmail.com>
2024-01-13 19:54:15 +0330

    replace component

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import { LandingLayout } from "@/layout/landing-layout/index.js";
-import PageContainer from "@/components/about-us/PageContainer";
+import { FirstTextContent, PageContainer } from "@/components/about-us";
 export default function About() {
   const [color, setColor] = useState("bg-teal-300");
   return (
@@ -9,7 +9,7 @@ export default function About() {
       <LandingLayout>
         {/* <BasePage fixed={true}> */}
         <PageContainer>
-          <div>first text</div>
+          <FirstTextContent />
           <section>warning box</section>
           <div>second text</div>
           <div>our team box</div>
