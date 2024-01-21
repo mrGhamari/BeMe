d9318a6867e2fbe10bb08e28b5365d8972eb9b8d
miveh <miveh@imivechi@gmail.com>
2024-01-22 01:21:30 +0330

    add content in component

--- src/components/about-us/index.js
+++ src/components/about-us/index.js
@@ -1,2 +1,5 @@
 export { default as PageContainer } from "./PageContainer";
 export { default as FirstTextContent } from "./FirstTextContent";
+export { default as WarningBox } from "./WarningBox";
+export { default as SecondTextContent } from "./SecondTextContent";
+export { default as CharityBox } from "./CharityBox";
