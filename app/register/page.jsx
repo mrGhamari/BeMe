26210dd0ae266843f97a537dc572e636504dcfbc
mrGhamari <mmdrza77@gmail.com>
2024-01-22 23:44:36 +0330

    debug

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-// import { RegisterForm } from "@/components/register";
-import RegisterForm from '../../src/components/register/register-form'
+import { RegisterForm } from "@/components/register";
+// import RegisterForm from '@/components/register/register-form'
 
 const Register = () => {
   return (
