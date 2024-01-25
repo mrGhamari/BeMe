47fd3a25214b8ca462cb07463d5cb2f1855f862d
dannymhmdi <dannymohammadi1377@gmail.com>
2024-01-25 11:14:32 +0330

    become guide created with guide page

---
+++ app/become-guide/page.jsx
@@ -0,0 +1,12 @@
+import { BecomeGuide } from '@/components/become-guide'
+import React from 'react'
+
+const page = () => {
+  return (
+     <>
+     <BecomeGuide></BecomeGuide>
+     </>
+  )
+}
+
+export default page
